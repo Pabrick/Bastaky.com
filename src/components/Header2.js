77637b5052912b5f900e07527537a3ad2fa048d0
Pabrick <pabrick.bastaky@gmail.com>
2018-11-26 22:14:40 +0100

    This is now a React Project

---
+++ src/components/Header2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header2 extends Component {
+    render() {
+        return (
+            <header className="app-header">
+                El componente Header
+            </header>
+        );
+    }
+}
+
+export default Header2;
