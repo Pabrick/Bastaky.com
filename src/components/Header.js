0623c469801942c6849307d3e5fb192f917e070f
Pabrick <pabrick.bastaky@gmail.com>
2018-12-08 21:37:20 +0100

    WIP: Header with style

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,15 +9,12 @@ class Header extends Component {
         return (
             <header className="app-header">
                 <div className="wrapper header-wrapper">
+                    <div className="header-pabrick"></div>
                     <h1>
                         <span className="h1-bast">BAST</span>
                         <span className="h1-aky">AKY<div className="h1-rotation">.COM</div></span>
                     </h1>
                     <button classMenu="header-bt-menu"></button>
-                </div>
-                <div className="header-bar">
-                    <div className="wrapper">
-                    </div>
                 </div>
             </header>
         );
