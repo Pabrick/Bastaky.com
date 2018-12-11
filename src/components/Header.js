e84523f577d3eb92b46b1dc9f9323b1acb507741
Pabrick <pabrick.bastaky@gmail.com>
2018-12-11 23:50:44 +0100

    +ADD: Header interaction

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,29 @@ import './../styles/Header.css';
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+    state = {
+        menuOpen: false
+    };
+
+    toogleMenu = (event) => {
+        event.preventDefault();
+        const currentState = this.state.menuOpen;
+        this.setState({ menuOpen: !currentState });
+    }
+
     render() {
-        const value = '10px';
-        console.log(value);
         return (
             <header className="app-header">
                 <div className="wrapper header-wrapper">
                     <div className="header-pabrick"></div>
                     <h1>
-                        <span className="h1-bast">BAST</span>
+                        <span className="h1-bast">BAST {this.currentState}</span>
                         <span className="h1-aky">AKY<div className="h1-rotation">.COM</div></span>
                     </h1>
-                    <button classMenu="header-bt-menu"></button>
+                    <button className={`header-bt-menu ${this.state.menuOpen ? 'open': 'close'}`} onClick={this.toogleMenu}>
+                        <span></span><span></span><span></span><span></span>
+                    </button>
                 </div>
             </header>
         );
